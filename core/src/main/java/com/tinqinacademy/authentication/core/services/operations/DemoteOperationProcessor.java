3f7ea400f3c2becc5e7a4ff1dcb4eefbcfef6974
martinfilkov <martinfilkov1@gmail.com>
2024-08-11 17:33:25 +0300

    Promote and Demote functionality

--- core/src/main/java/com/tinqinacademy/authentication/core/services/operations/DemoteOperationProcessor.java
+++ core/src/main/java/com/tinqinacademy/authentication/core/services/operations/DemoteOperationProcessor.java
@@ -1,17 +1,30 @@
 package com.tinqinacademy.authentication.core.services.operations;
 
 import com.tinqinacademy.authentication.api.operations.base.Errors;
+import com.tinqinacademy.authentication.api.operations.exceptions.NotFoundException;
 import com.tinqinacademy.authentication.api.operations.operations.demote.DemoteUserInput;
 import com.tinqinacademy.authentication.api.operations.operations.demote.DemoteUserOperation;
 import com.tinqinacademy.authentication.api.operations.operations.demote.DemoteUserOutput;
+import com.tinqinacademy.authentication.api.operations.operations.promote.PromoteUserInput;
+import com.tinqinacademy.authentication.api.operations.operations.promote.PromoteUserOutput;
 import com.tinqinacademy.authentication.core.ErrorMapper;
 import com.tinqinacademy.authentication.core.services.BaseOperationProcessor;
+import com.tinqinacademy.authentication.persistence.entities.User;
+import com.tinqinacademy.authentication.persistence.models.RoleType;
 import com.tinqinacademy.authentication.persistence.repositories.UserRepository;
 import io.vavr.control.Either;
+import io.vavr.control.Try;
 import jakarta.validation.Validator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static io.vavr.API.*;
+import static io.vavr.Predicates.instanceOf;
 
 @Slf4j
 @Service
@@ -29,10 +42,32 @@ public class DemoteOperationProcessor extends BaseOperationProcessor implements 
     @Override
     public Either<Errors, DemoteUserOutput> process(DemoteUserInput input) {
         return validateInput(input)
-                .flatMap(validated -> promoteUser(input));
+                .flatMap(validated -> demoteUser(input));
     }
 
-    private Either<Errors, DemoteUserOutput> promoteUser(DemoteUserInput input) {
-        return null;
+    private Either<Errors, DemoteUserOutput> demoteUser(DemoteUserInput input) {
+        return Try.of(() -> {
+                    log.info("Start demoteUser with input: {}", input);
+                    User user = getUserIfExists(input);
+                    user.setRoleType(RoleType.USER);
+                    userRepository.save(user);
+
+                    DemoteUserOutput output = DemoteUserOutput.builder().build();
+                    log.info("End demoteUser with output: {}", output);
+                    return output;
+                })
+                .toEither()
+                .mapLeft(throwable -> Match(throwable).of(
+                        Case($(instanceOf(NotFoundException.class)), ex -> errorMapper.handleError(ex, HttpStatus.NOT_FOUND)),
+                        Case($(), ex -> errorMapper.handleError(ex, HttpStatus.BAD_REQUEST))
+                ));
+    }
+
+    private User getUserIfExists(DemoteUserInput input) {
+        Optional<User> userOptional = userRepository.findById(UUID.fromString(input.getUserId()));
+        if (userOptional.isEmpty()) {
+            throw new NotFoundException(String.format("User with id %s not found", input.getUserId()));
+        }
+        return userOptional.get();
     }
 }
--- core/src/main/java/com/tinqinacademy/authentication/core/services/operations/PromoteOperationProcessor.java
+++ core/src/main/java/com/tinqinacademy/authentication/core/services/operations/PromoteOperationProcessor.java
@@ -1,17 +1,28 @@
 package com.tinqinacademy.authentication.core.services.operations;
 
 import com.tinqinacademy.authentication.api.operations.base.Errors;
+import com.tinqinacademy.authentication.api.operations.exceptions.NotFoundException;
 import com.tinqinacademy.authentication.api.operations.operations.promote.PromoteUserInput;
 import com.tinqinacademy.authentication.api.operations.operations.promote.PromoteUserOperation;
 import com.tinqinacademy.authentication.api.operations.operations.promote.PromoteUserOutput;
 import com.tinqinacademy.authentication.core.ErrorMapper;
 import com.tinqinacademy.authentication.core.services.BaseOperationProcessor;
+import com.tinqinacademy.authentication.persistence.entities.User;
+import com.tinqinacademy.authentication.persistence.models.RoleType;
 import com.tinqinacademy.authentication.persistence.repositories.UserRepository;
 import io.vavr.control.Either;
+import io.vavr.control.Try;
 import jakarta.validation.Validator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static io.vavr.API.*;
+import static io.vavr.Predicates.instanceOf;
 
 @Slf4j
 @Service
@@ -33,6 +44,28 @@ public class PromoteOperationProcessor extends BaseOperationProcessor implements
     }
 
     private Either<Errors, PromoteUserOutput> promoteUser(PromoteUserInput input) {
-        return null;
+        return Try.of(() -> {
+                    log.info("Start promoteUser with input: {}", input);
+                    User user = getUserIfExists(input);
+                    user.setRoleType(RoleType.ADMIN);
+                    userRepository.save(user);
+
+                    PromoteUserOutput output = PromoteUserOutput.builder().build();
+                    log.info("End promoteUser with output: {}", output);
+                    return output;
+                })
+                .toEither()
+                .mapLeft(throwable -> Match(throwable).of(
+                        Case($(instanceOf(NotFoundException.class)), ex -> errorMapper.handleError(ex, HttpStatus.NOT_FOUND)),
+                        Case($(), ex -> errorMapper.handleError(ex, HttpStatus.BAD_REQUEST))
+                ));
+    }
+
+    private User getUserIfExists(PromoteUserInput input) {
+        Optional<User> userOptional = userRepository.findById(UUID.fromString(input.getUserId()));
+        if (userOptional.isEmpty()) {
+            throw new NotFoundException(String.format("User with id %s not found", input.getUserId()));
+        }
+        return userOptional.get();
     }
 }
